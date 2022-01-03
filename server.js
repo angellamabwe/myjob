b41b1d9f260e8c85d59d202d38b180164cd66c2c
Angella Mabwegit <angellamabwe@gmail.com>
2022-01-03 11:38:29 +0200

    author routes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 const indexRouter = require('./routes/index') //hooking up server to use my routes step 1- importing the route into our server
+const authorRouter = require('./routes/authors')
 
 // configuring Express application
 app.set('view engine', 'ejs')
@@ -21,7 +22,10 @@ app.set('views', __dirname + '/views') //setting where our views will be coming 
 app.set('layout', 'layouts/layout') // hook up express layout
 app.use(expressLayouts)
 app.use(express.static('public')) //telling express where our public files(eg style sheets, JS files, images) are going to be
+app.use(express.urlencoded({ limit: '10mb', extended: false }))
+app.use(express.json())
 
 app.use('/', indexRouter) //hooking up server to use my routes step 2
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
